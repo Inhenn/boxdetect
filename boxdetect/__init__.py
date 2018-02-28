a1c7d14cbb5ec4006a4c432bf384704f58dbdc19
Inhenn <Reenhance96@gmail.com>
2018-02-28 11:32:53 -0500

    Update __init__.py

--- boxdetect/__init__.py
+++ boxdetect/__init__.py
@@ -243,7 +243,7 @@ def box_draw(img,left_x1,left_x2,left_y1,left_y2,top_x1,top_x2,top_y1,top_y2,rig
         print("detection failed, please try more lines")
         return 0
 
-def box_crop(img, left_x1, left_x2, left_y1, left_y2, top_x1, top_x2, top_y1, top_y2, right_x1, right_x2, right_y1,
+def box_chop(img, left_x1, left_x2, left_y1, left_y2, top_x1, top_x2, top_y1, top_y2, right_x1, right_x2, right_y1,
              right_y2, bot_x1,
              bot_x2, bot_y1, bot_y2, line_num=None, left_length=None, right_length=None, top_length=None,
              bot_length=None, accuracy=None):
